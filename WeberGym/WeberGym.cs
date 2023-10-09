5329fe927ae6bcd1ba55ef8c0a2c9abd0aee1f12
Christian Luke <13chrisl@gmail.com>
2023-10-09 01:15:04 -0600

    Final Submission

--- WeberGym/WeberGym.cs
+++ WeberGym/WeberGym.cs
@@ -3,8 +3,8 @@
  * Date: 10/8/2023
  * Description: This program performs compile-time initilization to generate tables for the number of attendees for
  * both Zumba and Spinning classes at the Weber Gym. Each class has four time slots and run 6 days a week.
- * This program generates 2D arrays and populates them with the number of attendees per class based on the time slot and day of the week.
- * Time slots are represented by the columns, while days of the week are represented by the rows.
+ * This program generates two 2D jagged arrays and populates them with the number of attendees per class based on the time slot
+ * and day of the week. Time slots are represented by the columns, while days of the week are represented by the rows.
  * After being populated these arrays are displayed for the user using nested for loops to iterate through the arrays for 
  * both the Zumba and Spinning classes. Each is clearly labeled showing the attendees at each time slot and day, while
  * also calculating the totals for each time slot for the entire week, totals for each day, and total attendees for every
@@ -14,7 +14,10 @@
  * Sources:
  * https://www.w3schools.com/cs/cs_properties.php
  * https://www.geeksforgeeks.org/string-format-method-in-c-sharp-with-examples-set-1/#
- * 
+ * https://dotnettutorials.net/lesson/two-dimensional-array-in-csharp/
+ * https://stackoverflow.com/questions/4260207/how-do-you-get-the-width-and-height-of-a-multi-dimensional-array
+ * https://www.tutorialspoint.com/Different-ways-for-Integer-to-String-Conversions-in-Chash
+ * https://www.w3resource.com/csharp-exercises/array/csharp-array-exercise-25.php
  */
 
 
